a84f745a29e828a5049852928254304e1a42dd63
Brian Schroer <schroer.brian@gmail.com>
2016-05-08 19:34:17 -0700

    beginnings of lightbox ui and logic.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 // 12:38
+// 2:22
+// 6:28
+// 7:28
+// can we use external css, resets, etc
 'use strict';
 
 (function() {
+  var imgModels  = {};
+  var lightboxImgEl;
+  var lightboxContainerEl;
+  var lightboxExit;
+  var lightboxVisibleClass = 'lightbox-visible';
+
+  // dom querying after window has loaded
+  window.onload = function() {
+    lightboxContainerEl = document.getElementById('js-lightbox-container');
+    lightboxImgEl       = document.getElementById('js-lightbox-current-image');
+    lightboxExit        = document.getElementById('js-lightbox-exit');
+
+    lightboxExit.onclick = function() {
+      lightboxContainerEl.classList.remove(lightboxVisibleClass);
+    }
+  }
+
   // image search endpoint
   var googleSearchBaseUrl = 'https://www.googleapis.com/customsearch/v1?';
   var apiKey = '5da789639b22c37d65c515690ff1242d';
@@ -14,10 +35,24 @@
   var parseResponse = function parseResponse(json) {
     if (json.items) {
       var imgObjects = json.items;
+      var containerEl = document.getElementById('js-imgs-container');
 
-      imgObjects.forEach(function(imgObj) {
-        console.log(imgObj.image.thumbnailLink);
-      })
+      imgObjects.forEach(function(imgObj, index) {
+        var imgEl  = document.createElement('div');
+        var imgThumb = imgObj.image.thumbnailLink;
+        var imgId    = 'imgItem' + index;
+
+        imgModels[imgId] = imgObj.link;
+        imgEl.id = imgId;
+        imgEl.classList.add('img-item');
+        imgEl.innerHTML = '<img src="' + imgThumb + '">';
+        containerEl.appendChild(imgEl);
+
+        imgEl.onclick = function() {
+          lightboxContainerEl.classList.add(lightboxVisibleClass);
+          lightboxImgEl.src = imgModels[this.id];
+        }
+      });
     } else {
       console.log('error parsing response');
     }
@@ -34,7 +69,6 @@
     if (xhr.readyState === DONE) {
       if (xhr.status === OK) {
         var parsedJson = JSON.parse(xhr.responseText);
-        debugger;
         parseResponse(parsedJson);
       } else {
         console.log('Error: ' + xhr.status);
